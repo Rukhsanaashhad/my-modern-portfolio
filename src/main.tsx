fb31b95fb20a71cb84ef2d7033a10203e53ee3b1
Rukhsanaashhad <maxtraders712@gmail.com>
2025-11-27 23:03:27 +0500

    🚀 Update: Added new features/fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './app.css'          // YE BILKUL SAHI HAI
+import './index.css'  
+
+import './App.css'    
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
